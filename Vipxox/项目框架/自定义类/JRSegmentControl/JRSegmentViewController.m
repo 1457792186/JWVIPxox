7fae19c385cca57075b661030fd9351874cb21aa
apple <apple@apples-iMac.lan>
2017-04-17 17:43:05 +0800

    Add project

---
+++ Vipxox/项目框架/自定义类/JRSegmentControl/JRSegmentViewController.m
@@ -0,0 +1,163 @@
+//
+//  JRSegmentViewController.m
+//  JRSegmentControl
+//
+//  Created by 湛家荣 on 15/8/29.
+//  Copyright (c) 2015年 湛家荣. All rights reserved.
+//
+
+#import "JRSegmentViewController.h"
+
+@interface JRSegmentViewController () <UIScrollViewDelegate, JRSegmentControlDelegate>
+{
+    CGFloat vcWidth;  // 每个子视图控制器的视图的宽
+    CGFloat vcHeight; // 每个子视图控制器的视图的高
+    
+    JRSegmentControl *segment;
+    
+    BOOL _isDrag;
+}
+
+@property (nonatomic, strong) UIScrollView *scrollView;
+
+@end
+
+@implementation JRSegmentViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.view.backgroundColor = [UIColor whiteColor];
+    
+    
+    [self setupScrollView];
+    [self setupViewControllersWithIndex:0];
+    [self setupSegmentControl];
+    
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+- (CGFloat)itemWidth
+{
+    if (_itemWidth == 0) {
+        _itemWidth = 60.0f;
+    }
+    return _itemWidth;
+}
+
+- (CGFloat)itemHeight
+{
+    if (_itemHeight == 0) {
+        _itemHeight = 30.0f;
+    }
+    return _itemHeight;
+}
+
+- (BOOL)shouldAutorotate
+{
+    return NO;
+}
+
+- (UIInterfaceOrientationMask)supportedInterfaceOrientations
+{
+    return UIInterfaceOrientationMaskPortrait;
+}
+
+- (UIInterfaceOrientation)preferredInterfaceOrientationForPresentation
+{
+    return UIInterfaceOrientationPortrait;
+}
+
+/** 设置scrollView */
+- (void)setupScrollView
+{
+    CGFloat Y = 0.0f;
+    if (self.navigationController != nil && ![self.navigationController isNavigationBarHidden]) {
+        self.automaticallyAdjustsScrollViewInsets = NO;
+        Y = 64.0f;
+    }
+    
+    vcWidth = self.view.frame.size.width;
+    vcHeight = self.view.frame.size.height - Y;
+    
+    UIScrollView *scrollView = [[UIScrollView alloc] initWithFrame:CGRectMake(0.0f, Y, vcWidth, vcHeight)];
+    scrollView.contentSize = CGSizeMake(vcWidth * self.viewControllers.count, vcHeight);
+    scrollView.showsHorizontalScrollIndicator = NO;
+    scrollView.showsVerticalScrollIndicator   = NO;
+    scrollView.pagingEnabled = YES;
+    scrollView.delegate      = self;
+    [self.view addSubview:scrollView];
+    self.scrollView = scrollView;
+}
+
+/** 设置子视图控制器，这个方法必须在viewDidLoad方法里执行，否则子视图控制器各项属性为空 */
+- (void)setupViewControllersWithIndex:(NSInteger)index{
+    if (![self.viewControllers[index] isKindOfClass:[UIViewController class]]) {
+        Class vcClass = self.viewControllers[index];
+        UIViewController * vc = [[vcClass alloc]init];
+        [self addChildViewController:vc];
+        vc.view.frame = CGRectMake(vcWidth * index, 0, vcWidth, vcHeight);
+        [self.scrollView addSubview:vc.view];
+        [self.viewControllers replaceObjectAtIndex:index withObject:vc];
+    }
+}
+
+/** 设置segment */
+- (void)setupSegmentControl
+{
+    _itemWidth = 60.0f;
+    // 设置titleView
+    segment = [[JRSegmentControl alloc] initWithFrame:CGRectMake(0, 0, _itemWidth * 3, 30.0f)];
+    segment.titles = self.titles;
+    segment.cornerRadius = 5.0f;
+    segment.titleColor = self.titleColor;
+    segment.indicatorViewColor = self.indicatorViewColor;
+    segment.backgroundColor = self.segmentBgColor;
+    
+    segment.delegate = self;
+    self.navigationItem.titleView = segment;
+}
+
+
+#pragma mark - UIScrollViewDelegate
+
+- (void)scrollViewWillBeginDragging:(UIScrollView *)scrollView {
+    [segment selectedBegan];
+    _isDrag = YES;
+}
+
+- (void)scrollViewDidScroll:(UIScrollView *)scrollView {
+    if (_isDrag) {
+        NSInteger index = scrollView.contentOffset.x/[UIScreen mainScreen].bounds.size.width;
+        [self setupViewControllersWithIndex:index];
+        CGFloat percent = scrollView.contentOffset.x / scrollView.contentSize.width;
+        
+        [segment setIndicatorViewPercent:percent];
+    }
+}
+
+- (void)scrollViewDidEndDragging:(UIScrollView *)scrollView willDecelerate:(BOOL)decelerate {
+    if (decelerate == NO) {
+        [segment selectedEnd];
+    }
+}
+
+- (void)scrollViewDidEndDecelerating:(UIScrollView *)scrollView {
+    NSInteger index = scrollView.contentOffset.x / scrollView.frame.size.width;
+    [segment setSelectedIndex:index];
+    _isDrag = NO;
+}
+
+#pragma mark - JRSegmentControlDelegate
+
+- (void)segmentControl:(JRSegmentControl *)segment didSelectedIndex:(NSInteger)index {
+    [self setupViewControllersWithIndex:index];
+    CGFloat X = index * self.view.frame.size.width;
+    [self.scrollView setContentOffset:CGPointMake(X, 0) animated:YES];
+}
+
+@end
